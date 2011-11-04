63f416bfac072073ff526cbd14cdcab69b017810
Mike Wallace <mikewallace1979@googlemail.com>
2011-11-04 11:56:59 +0000

    Initial support for pie charts.

--- backbone-d3.js
+++ backbone-d3.js
@@ -15,16 +15,46 @@
         draw: function() {
           // Draw the plot
           var data = this.collection.plotdata();
-          this[this.collection.plottype](data);
+          this[this.collection.plottype](data, {newPlot: true});
         },
         redraw: function(){
           // transition the plot
-
+          var that = this;
+          this.collection.fetch({silent: true, success: function() {
+            var data = that.collection.plotdata();
+            that[that.collection.plottype](data, {newPlot: false});
+          }});
         },
-        pie: function(data){
-
+        pie: function(data, options){
+          var m = 10, r = 100, z = d3.scale.category20c();
+          if (options.newPlot) {
+            var svg = d3.select("#plot").selectAll("svg")
+                .data([data])
+              .enter().append("svg:svg")
+                .attr("width", (r + m) * 2)
+                .attr("height", (r + m) * 2)
+              .append("svg:g")
+                .attr("transform", "translate(" + (r + m) + "," + (r + m) + ")");
+            svg.selectAll("path")
+              .data(d3.layout.pie())
+              .enter().append("svg:path")
+                .attr("d", d3.svg.arc()
+                  .innerRadius(r / 8)
+                  .outerRadius(r))
+                .style("fill", function(d, i) { return z(i);});
+          } else {
+            var svg = d3.select("#plot").selectAll("svg");
+            svg.data([data]);
+            var pie = svg.selectAll("path");
+            pie.data(d3.layout.pie());
+            pie.transition()
+              .attr("d", d3.svg.arc()
+                .innerRadius(r / 8)
+                .outerRadius(r))
+              .style("fill", function(d, i) { return z(i);});
+          }
         },
-        bar: function(data){
+        bar: function(data, options){
 
         }
       }
