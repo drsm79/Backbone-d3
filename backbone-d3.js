8855222066480e099921b4812538e4d08a6b223b
Simon Metson <simonmetson@googlemail.com>
2011-11-08 08:44:32 +0000

    Working bar example
    Tweaked the core code to allow for better configuration, bit less
    redundant code

--- backbone-d3.js
+++ backbone-d3.js
@@ -4,34 +4,35 @@
       {
         initialize: function(collection, settings) {
           _.bindAll(this);
-          this.settings = settings;
           this.collection = collection;
           this.collection.bind('change', this.redraw);
           this.collection.bind('add', this.redraw);
           this.collection.bind('remove', this.redraw);
           this.collection.bind('reset', this.draw);
-          this.collection.fetch();
+
+          this.settings = settings || {};
+					this.div = this.settings.div || d3.select("#plot");
+
+					this.collection.fetch();
 					// TODO: make the chart a member var of the View
 					// for easier access/fine grained control
         },
-        draw: function() {
-          // Draw the plot
-          var data = this.collection.plotdata();
-          this[this.collection.plottype](data, {newPlot: true});
-		  		//this.caption();
+				draw: function() {
+					// Draw the plot
+					if (this.collection.plotdata().length > 0) {
+						this[this.collection.plottype]({newPlot: true});
+		  			//this.caption();
+					}
         },
         redraw: function(){
           // transition the plot
-          var that = this;
-          this.collection.fetch({silent: true, success: function() {
-            var data = that.collection.plotdata();
-            that[that.collection.plottype](data, {newPlot: false});
-          }});
+					this[this.collection.plottype]({newPlot: false});
         },
-        pie: function(data, options){
+        pie: function(options){
+          var data = this.collection.plotdata();
           var m = 10, r = 100, z = d3.scale.category20c();
           if (options.newPlot) {
-            var svg = d3.select("#plot").selectAll("svg")
+            var svg = this.div.selectAll("svg")
                 .data([data])
               .enter().append("svg:svg")
                 .attr("width", (r + m) * 2)
@@ -57,23 +58,23 @@
               .style("fill", function(d, i) { return z(i);});
           }
         },
-        bar: function(data, options){
+        bar: function(options){
 	        var w = options.w || 20;
 					var h = options.h || 80;
-					data = this.collection.plotdata();
+          var data = this.collection.plotdata();
+					var scale = h/_.max(data);
+					console.log(scale);
+					console.log(data.length);
+					var x = d3.scale.linear()
+						.domain([0, 1])
+						.range([0, w]);
+
+					var y = d3.scale.linear()
+						.domain([0, 100])
+						.rangeRound([0, h]);
+
           if (options.newPlot) {
-						console.log('hello');
-
-						var x = d3.scale.linear()
-							.domain([0, 1])
-							.range([0, w]);
-
-						var y = d3.scale.linear()
-							.domain([0, 100])
-							.rangeRound([0, h]);
-
-						var chart = d3.select("#plot")
-							.append("svg:svg")
+						var chart = this.div.append("svg:svg")
 								.attr("class", "chart")
 								.attr("width", w * data.length - 1)
 								.attr("height", h);
@@ -82,25 +83,32 @@
 								.data(data)
 							.enter().append("svg:rect")
 								.attr("x", function(d, i) { return x(i) - .5; })
-								.attr("y", function(d) { return h - y(d) - .5; })
+								.attr("y", function(d) { return h - scale * y(d) - .5; })
 								.attr("width", w)
-								.attr("height", function(d) { return y(d); });
+								.attr("height", function(d) { return scale * y(d); });
 
 						chart.append("svg:line")
 							.attr("x1", 0)
-							.attr("x2", w * data.length)
+							.attr("x2", w * (1 + data.length))
 							.attr("y1", h - .5)
 							.attr("y2", h - .5)
 							.attr("stroke", "#000");
 
-						console.log('woot');
+						chart.append("svg:line")
+							.attr("x1", 0)
+							.attr("x2", 0)
+							.attr("y1", 0)
+							.attr("y2", h - .5)
+							.attr("stroke", "#000");
+					} else if (options.scrolling){
+						console.log('scrolling, yo!');
 					} else {
-						d3.select("#plot").selectAll("rect")
-							.data(data)
-							.transition()
-							.duration(1000)
-							.attr("y", function(d) { return h - y(d) - .5; })
-							.attr("height", function(d) { return y(d); });
+						this.div.selectAll("rect")
+								.data(data)
+						.transition()
+							.duration(500)
+							.attr("y", function(d) { return h - scale * y(d) - .5; })
+							.attr("height", function(d) { return scale * y(d); });
 					}
         }	// ,
         	// 		caption: function(){
